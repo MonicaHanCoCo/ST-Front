8dd8d1fb70dbba9c00864d83d9cad9ee7c553e0a
mogiven <2359353909@qq.com>
2023-06-11 22:30:34 +0800

    final

--- src/components/LogQuery.vue
+++ src/components/LogQuery.vue
@@ -1,8 +1,9 @@
 <template>
     <el-button @click="search">搜索</el-button>
+    <br><br>
     <el-table :data="logs" style="width: 100%">
-        <el-table-column prop="query" label="SQL Query"></el-table-column>
-        <el-table-column prop="time" label="Query Time"></el-table-column>
+        <el-table-column prop="sql" label="SQL Query"></el-table-column>
+        <el-table-column prop="timestamp" label="Query Time"></el-table-column>
     </el-table>
 </template>
 <script setup>
@@ -12,23 +13,11 @@ const logs = ref([]);
 const search = async () => {
     try {
         const response = await axios.get('/log');
-        console.log("reslog", response);
+        console.log("reslog", response.data.data);
+        logs.value = response.data.data;
     } catch (error) {
         console.error(error);
     }
-};
-// 模拟获取查询日志的函数
-function fetchQueryLogs() {
-    // 发起异步请求获取查询日志数据
-    // 假设返回的数据格式为 [{ query: 'SELECT * FROM users', time: '2023-06-01 14:30:00' }, ...]
-    const response = [
-        { query: 'SELECT * FROM users', time: '2023-06-01 14:30:00' },
-        { query: 'SELECT * FROM products', time: '2023-06-01 14:31:00' },
-        { query: 'SELECT * FROM orders', time: '2023-06-01 14:32:00' }
-    ];
-    logs.value = response;
-}
 
-// 在组件挂载时获取查询日志数据
-fetchQueryLogs();
+};
 </script>
--- src/components/Recommend.vue
+++ src/components/Recommend.vue
@@ -6,8 +6,8 @@
     <br>
     <h4>Recommended News :</h4><br>
     <el-table :data="recommendData" style="width: 100%">
-        <el-table-column prop="rid" label="新闻编号"></el-table-column>
-        <el-table-column prop="category" label="新闻主题"></el-table-column>
+        <el-table-column prop="newsId" label="新闻编号"></el-table-column>
+        <el-table-column prop="topic" label="新闻主题"></el-table-column>
         <el-table-column prop="title" label="新闻标题"></el-table-column>
         <el-table-column prop="content" label="新闻内容"></el-table-column>
     </el-table>
@@ -15,10 +15,14 @@
 <script setup>
 import { ref, inject } from 'vue'
 const axios = inject("$axios");
+const recommendData = ref([]);
 const userId = ref('');
 const search = async () => {
     try {
-        const response = await axios.post('/real-time/set-user', { userId: userId.value });
+        const response = await axios.get('/real-time/set-user', { 
+            params: {
+                userId: userId.value
+            }});
         console.log("res", response);
     } catch (error) {
         console.error(error);
@@ -33,44 +37,28 @@ if ('WebSocket' in window) {
 } else {
     alert('浏览器不支持webSocket');
 }
-//连接发生错误的回调方法
-websocket.onerror = function () {
-    setMessageInnerHTML("error");
-};
+  //连接发生错误的回调方法
+  websocket.onerror = function () {
+        setMessageInnerHTML("error");
+    };
+    
+    //连接成功建立的回调方法
+    websocket.onopen = function (event) {
+        setMessageInnerHTML("open");
+    };
 
 //接收到消息的回调方法
 websocket.onmessage = function (event) {
     let data = event.data;
     console.log("实时数据:" + data);
-
+    recommendData.value = JSON.parse(data); 
 };
-const recommendData = ref([
-    {
-        rid: '01',
-        topic: 'Topic A',
-        title: 'Title A',
-        content: 'Content A',
-    },
-    {
-        rid: '02',
-        topic: 'Topic A',
-        title: 'Title A',
-        content: 'Content A',
-    },
-    {
-        rid: '03',
-        topic: 'Topic A',
-        title: 'Title A',
-        content: 'Content A',
-    },
-    {
-        rid: '04',
-        date: '2016-05-01',
-        topic: 'Topic D',
-        title: 'Title D',
-        content: 'Content D',
-    },
-]);
+ //连接关闭的回调方法
+ websocket.onclose = function () {
+        setMessageInnerHTML("close");
+    };
+
+
 </script>
 <style lang="scss" scoped>
 .search-container {
